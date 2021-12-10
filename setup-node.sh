89bd4bcfad541396cbf1693d2864c9289e32a2db
Foyzul <foyzulkarim@gmail.com>
2021-12-10 23:45:26 +1100

    Split setup.sh file

---
+++ setup-node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nvm install node
+npm i -g npm@latest
+npm install -g zx
---
+++ setup-nvm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# My first script
+echo "Setting up nvm!"
+sudo apt install curl 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
