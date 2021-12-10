b4b81d5e5994a54aad3c7775fa7a8982e5047e62
Foyzul <foyzulkarim@gmail.com>
2021-12-11 01:25:02 +1100

    Update nvm

--- setup-nvm.sh
+++ setup-nvm.sh
@@ -3,3 +3,9 @@
 echo "Setting up nvm!"
 sudo apt install curl 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+npm i -g npm@latest
+npm install -g zx
